174d5a74ede96c99a8387d22be768352c6c487e7
Amy de Buitléir <amy@nualeargais.ie>
2014-01-07 17:16:37 +0000

    v0.1.1

--- src/ALife/Realtra/GeneratePopulation.hs
+++ src/ALife/Realtra/GeneratePopulation.hs
@@ -16,9 +16,10 @@ import ALife.Realtra.Wain (Astronomer, randomAstronomer)
 import qualified ALife.Realtra.Config as Config
 import ALife.Creatur.Logger (writeToLog)
 import ALife.Creatur.Universe (SimpleUniverse, addAgent)
-import ALife.Creatur.Wain.Pretty (pretty)
-import ALife.Creatur.Wain.Statistics (stats, maxStats, minStats, avgStats)
-import Control.Monad.Random (evalRandIO, Rand, RandomGen)
+-- import ALife.Creatur.Wain.Pretty (pretty)
+-- import ALife.Creatur.Wain.Statistics (stats, maxStats, minStats, avgStats)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Random (evalRandIO)
 import Control.Monad.State.Lazy (StateT, evalStateT)
 
 -- buildAgents :: DiploidReader [Astronomer]
@@ -26,24 +27,31 @@ import Control.Monad.State.Lazy (StateT, evalStateT)
 --   agents <- mapM (buildWain True) names
 --   return . catEithers $ agents
 
-buildAgents :: RandomGen r => Rand r [Astronomer]
-buildAgents = mapM randomAstronomer names
+-- buildAgents :: RandomGen r => Rand r [Astronomer]
+-- buildAgents = mapM randomAstronomer names
 
 names :: [String]
 names = map (("Founder" ++) . show) [1..Config.initialPopulationSize]
 
-introduce :: Astronomer -> StateT (SimpleUniverse Astronomer) IO ()
-introduce agent = do
+-- introduce :: Astronomer -> StateT (SimpleUniverse Astronomer) IO ()
+-- introduce agent = do
+--   writeToLog $ "GeneratePopulation: Created " ++ agentId agent
+--   addAgent agent
+
+-- introduceAll :: [Astronomer] -> StateT (SimpleUniverse Astronomer) IO ()
+-- introduceAll agents = do
+--   mapM_ introduce agents
+--   let xs = map stats agents
+--   writeToLog $ pretty (maxStats xs)
+--   writeToLog $ pretty (minStats xs)
+--   writeToLog $ pretty (avgStats xs)
+
+introduceRandomAgent :: String -> StateT (SimpleUniverse Astronomer) IO ()
+introduceRandomAgent name = do
+  agent <- liftIO $ evalRandIO (randomAstronomer name)
   writeToLog $ "GeneratePopulation: Created " ++ agentId agent
   addAgent agent
-
-introduceAll :: [Astronomer] -> StateT (SimpleUniverse Astronomer) IO ()
-introduceAll agents = do
-  mapM_ introduce agents
-  let xs = map stats agents
-  writeToLog $ pretty (maxStats xs)
-  writeToLog $ pretty (minStats xs)
-  writeToLog $ pretty (avgStats xs)
+  return ()
   
 main :: IO ()
 main = do
@@ -56,8 +64,9 @@ main = do
   -- let agents = runDiploidReader buildAgents (g1, g2)
 
   print names
-  agents <- evalRandIO buildAgents
-  evalStateT (introduceAll agents) Config.universe
+  -- agents <- evalRandIO buildAgents
+  -- evalStateT (introduceAll agents) Config.universe
+  evalStateT (mapM_ introduceRandomAgent names) Config.universe
 
 -- import ALife.Realtra.Wain
 -- import Control.Monad.Random
