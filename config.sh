513b29ad19b99e1f013b8c571528983571f35697
Amy de Buitléir <amy@nualeargais.ie>
2014-08-12 15:27:47 +0100

    v1.1.2

--- config.sh
+++ config.sh
@@ -3,3 +3,5 @@ export alifedir=~/alife/gzoo1
 export logdir=${alifedir}/log
 export logname=GalaxyZoo.log
 export log=${logdir}/${logname}
+export statsname=rawStats
+export stats=${alifedir}/${statsname}
--- summarise.sh
+++ summarise.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
-logs=`./ordered-logs.sh`
 
-extractStats ()
-{
-  key=$1
-  shift
-  files=$*
-  cat ${files} | grep "${key}" | head -n 2 | tail -n 1 | sed 's/.*\tSummary - /time,/; s/=[^,]*//g'
-  cat ${files} | grep "${key}" | sed 's/[0-9+]*\t//; s/\tSummary - /,/; s/,[^=]*=/,/g'
-}
+sed 's/=[^=]*,/,/g; s/=[^=]*$//' $stats | uniq
+sed 's/,[^,]*=/,/g; s/^[^,]*=//' $stats
 
-extractStats "Summary - max. " ${logs} > max.csv
-extractStats "Summary - min. " ${logs} > min.csv
-extractStats "Summary - avg. " ${logs} > avg.csv
-extractStats "Summary - std. dev. " ${logs} > stdDev.csv
-extractStats "Summary - total " ${logs} > total.csv
+# logs=`./ordered-logs.sh`
 
-join -j 1 --header -t, avg.csv max.csv | join -j 1 --header -t, - min.csv| join -j 1 --header -t, - stdDev.csv | join -j 1 --header -t, - total.csv | grep -v '^0,' > monster.csv
+# extractStats ()
+# {
+#   key=$1
+#   shift
+#   files=$*
+#   cat ${files} | grep "${key}" | head -n 2 | tail -n 1 | sed 's/.*\tSummary - /time,/; s/=[^,]*//g'
+#   cat ${files} | grep "${key}" | sed 's/[0-9+]*\t//; s/\tSummary - /,/; s/,[^=]*=/,/g'
+# }
+
+# extractStats "Summary - max. " ${logs} > max.csv
+# extractStats "Summary - min. " ${logs} > min.csv
+# extractStats "Summary - avg. " ${logs} > avg.csv
+# extractStats "Summary - std. dev. " ${logs} > stdDev.csv
+# extractStats "Summary - total " ${logs} > total.csv
+
+# join -j 1 --header -t, avg.csv max.csv | join -j 1 --header -t, - min.csv| join -j 1 --header -t, - stdDev.csv | join -j 1 --header -t, - total.csv | grep -v '^0,' > monster.csv
+
+
+
